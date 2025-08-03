a9734615903fb8d573ff1b37fa5e633f97552302
aldairm <dyalibatm@miumg.edu.gt>
2025-08-02 23:05:32 -0600

    Agregue la función de suma

--- Program.cs
+++ Program.cs
@@ -12,8 +12,34 @@ class Program
         Console.Write("Seleccione una opción: ");
 
         int opcion = int.Parse(Console.ReadLine());
-        // TODO: Implementar la lógica de menú
+
+        switch(opcion)
+        {
+            case 1:
+                Sumar();
+                break;
+            case 2:
+                Console.WriteLine("Función de resta pendiente.");
+                break;
+            case 3:
+                Console.WriteLine("Función de multiplicación pendiente.");
+                break;
+            case 4:
+                Console.WriteLine("Función de división pendiente.");
+                break;
+            default:
+                Console.WriteLine("Opción no válida.");
+                break;
+        }
     }
 
-    // TODO: Implementar funciones de suma, resta, multiplicación, división
+    static void Sumar()
+    {
+        Console.Write("Ingrese el primer número: ");
+        double num1 = double.Parse(Console.ReadLine());
+        Console.Write("Ingrese el segundo número: ");
+        double num2 = double.Parse(Console.ReadLine());
+        double resultado = num1 + num2;
+        Console.WriteLine($"El resultado de la suma es: {resultado}");
+    }
 }
